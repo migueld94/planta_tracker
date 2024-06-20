18d9856b8efd94f0e13d8993f1d3aeedfc2c9fd3
Yaidel Luis Araujo Collado <68750867+YaidelLuis@users.noreply.github.com>
2024-06-20 23:05:18 +0200

    cambiado el service a http.get

--- lib/services/all_plants_services.dart
+++ lib/services/all_plants_services.dart
@@ -24,18 +24,18 @@ class AllPlantServices {
     final Map<String, dynamic> data = json.decode(resp.body);
     final accessToken = data["access_token"];
 
-    final allPinPlant = Uri.parse('${Constants.baseUrl}/en/api/plants_map_api');
-
-    final response = await http.post(allPinPlant, headers: <String, String>{
-      'authorization': "Bearer $accessToken"
-    }, body: {
+    final allPinPlant =
+        Uri.https('${Constants.baseUrl}/en/api/plants_map_api', '', {
       "longitud_maxima": '$longMax',
       "longitud_minima": '$longMin',
       "latitud_maxima": '$latMax',
       "latitud_minima": '$latMin',
     });
 
-    // final utf = const Utf8Decoder().convert(resp.body.codeUnits);
+    final response = await http.get(
+      allPinPlant,
+      headers: <String, String>{'authorization': "Bearer $accessToken"},
+    );
 
     if (response.statusCode == 200) {
       final List<dynamic> data = json.decode(response.body);
@@ -53,3 +53,54 @@ class AllPlantServices {
     return [];
   }
 }
+
+
+// class AllPlantServices {
+//   var secretUrl = Uri.parse('${Constants.baseUrl}/en/api/o/token/');
+//   List<Plant> plants = [];
+
+//   Future<List<Plant>> getAllPin(
+//       double latMax, double latMin, double longMax, double longMin) async {
+//     String client = 'IMIUgjEXwzviJeCfVzCQw4g8GkhUpYGbcDieCxSE';
+//     String secret =
+//         'rOsMV2OjTPs89ku5NlWuukWNMfm9CDO3nZuzOxRWYCPUSSxnZcCfUl8XnU1HcPTfCqCTpZxYhv3zNYUB0H1hlQ6b7heLWsoqgJjLSkwAsZp7NTwT2B1D8nwfTS6bfvpw';
+//     String basicAuth = 'Basic ${base64.encode(utf8.encode('$client:$secret'))}';
+
+//     var resp = await http.post(secretUrl, headers: <String, String>{
+//       'authorization': basicAuth
+//     }, body: {
+//       "grant_type": "client_credentials",
+//     });
+
+//     final Map<String, dynamic> data = json.decode(resp.body);
+//     final accessToken = data["access_token"];
+
+//     final allPinPlant = Uri.parse('${Constants.baseUrl}/en/api/plants_map_api');
+
+//     final response = await http.post(allPinPlant, headers: <String, String>{
+//       'authorization': "Bearer $accessToken"
+//     }, body: {
+//       "longitud_maxima": '$longMax',
+//       "longitud_minima": '$longMin',
+//       "latitud_maxima": '$latMax',
+//       "latitud_minima": '$latMin',
+//     });
+
+//     // final utf = const Utf8Decoder().convert(resp.body.codeUnits);
+
+//     if (response.statusCode == 200) {
+//       final List<dynamic> data = json.decode(response.body);
+//       plants = data.map((json) => Plant.fromJson(json)).toList();
+//       return plants;
+//     } else {
+//       throw Exception('Something Error');
+//     }
+//   }
+
+//   Future<List<Plant>> getPinsByBoundries(
+//     LatLng northEast,
+//     LatLng southWest,
+//   ) async {
+//     return [];
+//   }
+// }
