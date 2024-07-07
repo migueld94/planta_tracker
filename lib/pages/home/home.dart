67a9f31e67f2340f6b95c466e5efb6d612734354
Miguel Angel LD <mdelgado2107@gmail.com>
2024-07-07 07:00:09 -0400

    update code ios

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -28,130 +28,138 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: AutoSizeText(
-            'Planta! Tracker',
-            style: context.theme.textTheme.titleApBar,
-          ),
-          centerTitle: true,
-          automaticallyImplyLeading: false,
-          backgroundColor: PlantaColors.colorGreen,
-          actions: [
-            IconButton(
-              icon: Icon(Icons.logout, color: PlantaColors.colorWhite),
-              tooltip: AppLocalizations.of(context)!.logout,
-              onPressed: () async {
-                warning(
-                  context,
-                  AppLocalizations.of(context)!.warning_exit,
-                  () async {
-                    EasyLoading.show();
-
-                    final token = await storage.read(key: 'token');
-                    var res = await authService.logout(token!);
-
-                    switch (res!.statusCode) {
-                      case 200:
-                        EasyLoading.dismiss();
-                        Navigator.pop(context);
-
-                        WidgetsBinding.instance.addPostFrameCallback((_) async {
-                          Navigator.push(
-                              context, SlideRightRoute(page: const Login()));
-                        });
-                        await storage.delete(key: 'token');
-                        break;
-                      case 400:
-                        EasyLoading.dismiss();
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: AutoSizeText(
-                              AppLocalizations.of(context)!.verify_credentials,
-                              style: context.theme.textTheme.text_01,
-                            ),
-                          ),
-                        );
-                        break;
-                      case 401:
-                        EasyLoading.dismiss();
-                        if (!context.mounted) return;
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: AutoSizeText(
-                              AppLocalizations.of(context)!.verify_credentials,
-                              style: context.theme.textTheme.text_01,
-                            ),
-                          ),
-                        );
-                        break;
-                      default:
-                        EasyLoading.dismiss();
-                        if (!context.mounted) return;
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: AutoSizeText(
-                              AppLocalizations.of(context)!.verify_credentials,
-                              style: context.theme.textTheme.text_01,
-                            ),
-                          ),
-                        );
-                        break;
-                    }
-                  },
-                );
-              },
+    return SafeArea(
+      top: false,
+      bottom: true,
+      child: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: AutoSizeText(
+              'Planta! Tracker',
+              style: context.theme.textTheme.titleApBar,
             ),
-          ],
-          bottom: TabBar(
-            indicatorColor: PlantaColors.colorOrange,
-            dividerColor: PlantaColors.colorWhite,
-            labelStyle: context.theme.textTheme.text_01
-                .copyWith(color: PlantaColors.colorWhite),
-            labelColor: PlantaColors.colorWhite,
-            tabs: [
-              Tab(
-                text: AppLocalizations.of(context)!.plants,
-                icon: const Icon(Ionicons.list_outline),
-              ),
-              Tab(
-                text: AppLocalizations.of(context)!.map,
-                icon: const Icon(Ionicons.map_outline),
-              ),
-              Tab(
-                text: AppLocalizations.of(context)!.my_plants,
-                icon: const Icon(Ionicons.leaf_outline),
+            centerTitle: true,
+            automaticallyImplyLeading: false,
+            backgroundColor: PlantaColors.colorGreen,
+            actions: [
+              IconButton(
+                icon: Icon(Icons.logout, color: PlantaColors.colorWhite),
+                tooltip: AppLocalizations.of(context)!.logout,
+                onPressed: () async {
+                  warning(
+                    context,
+                    AppLocalizations.of(context)!.warning_exit,
+                    () async {
+                      EasyLoading.show();
+
+                      final token = await storage.read(key: 'token');
+                      var res = await authService.logout(token!);
+
+                      switch (res!.statusCode) {
+                        case 200:
+                          EasyLoading.dismiss();
+                          Navigator.pop(context);
+
+                          WidgetsBinding.instance
+                              .addPostFrameCallback((_) async {
+                            Navigator.push(
+                                context, SlideRightRoute(page: const Login()));
+                          });
+                          await storage.delete(key: 'token');
+                          break;
+                        case 400:
+                          EasyLoading.dismiss();
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: AutoSizeText(
+                                AppLocalizations.of(context)!
+                                    .verify_credentials,
+                                style: context.theme.textTheme.text_01,
+                              ),
+                            ),
+                          );
+                          break;
+                        case 401:
+                          EasyLoading.dismiss();
+                          if (!context.mounted) return;
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: AutoSizeText(
+                                AppLocalizations.of(context)!
+                                    .verify_credentials,
+                                style: context.theme.textTheme.text_01,
+                              ),
+                            ),
+                          );
+                          break;
+                        default:
+                          EasyLoading.dismiss();
+                          if (!context.mounted) return;
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: AutoSizeText(
+                                AppLocalizations.of(context)!
+                                    .verify_credentials,
+                                style: context.theme.textTheme.text_01,
+                              ),
+                            ),
+                          );
+                          break;
+                      }
+                    },
+                  );
+                },
               ),
             ],
-          ),
-        ),
-        body: Center(
-          child: PopScope(
-            canPop: false,
-            onPopInvoked: (bool didPop) async {
-              if (!didPop) {
-                warning(
-                  context,
-                  AppLocalizations.of(context)!.warning_exit,
-                  () async {
-                    await storage.deleteAll();
-                    Navigator.pop(context);
-                    WidgetsBinding.instance.addPostFrameCallback((_) {
-                      Navigator.push(
-                          context, SlideRightRoute(page: const Login()));
-                    });
-                  },
-                );
-              }
-            },
-            child: const TabBarView(
-              children: [
-                AllPlants(),
-                MapView(),
-                MyPlants(),
+            bottom: TabBar(
+              indicatorColor: PlantaColors.colorOrange,
+              dividerColor: PlantaColors.colorWhite,
+              labelStyle: context.theme.textTheme.text_01
+                  .copyWith(color: PlantaColors.colorWhite),
+              labelColor: PlantaColors.colorWhite,
+              tabs: [
+                Tab(
+                  text: AppLocalizations.of(context)!.plants,
+                  icon: const Icon(Ionicons.list_outline),
+                ),
+                Tab(
+                  text: AppLocalizations.of(context)!.map,
+                  icon: const Icon(Ionicons.map_outline),
+                ),
+                Tab(
+                  text: AppLocalizations.of(context)!.my_plants,
+                  icon: const Icon(Ionicons.leaf_outline),
+                ),
               ],
+            ),
+          ),
+          body: Center(
+            child: PopScope(
+              canPop: false,
+              onPopInvoked: (bool didPop) async {
+                if (!didPop) {
+                  warning(
+                    context,
+                    AppLocalizations.of(context)!.warning_exit,
+                    () async {
+                      await storage.deleteAll();
+                      Navigator.pop(context);
+                      WidgetsBinding.instance.addPostFrameCallback((_) {
+                        Navigator.push(
+                            context, SlideRightRoute(page: const Login()));
+                      });
+                    },
+                  );
+                }
+              },
+              child: const TabBarView(
+                children: [
+                  AllPlants(),
+                  MapView(),
+                  MyPlants(),
+                ],
+              ),
             ),
           ),
         ),
